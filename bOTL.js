41ae99fbdf407753c96785c0778f4fd104da617e
Emlyn <emlynoregan@gmail.com>
2015-08-18 11:17:50 +0930

    First version of bOTL 3 for js, with tests

---
+++ bOTL.js
@@ -0,0 +1,335 @@
+var transform = function(aMASSource, abOTLTransform)
+{
+    //bOTL transform external method
+    var retval;
+
+    var lscope = {
+        "$": aMASSource,
+        "@": aMASSource
+    }
+    
+    var lresults = _transform(lscope, abOTLTransform)
+    
+    if (lresults["results"].length)
+        retval = lresults["results"][0]
+    else
+        retval = null;
+
+    return retval
+}
+    
+var _transform = function(aScope, abOTLTransform)
+{
+    //transform using populated scope
+
+    var lsectionF = GetSectionFunction(abOTLTransform)
+    
+    var lresultsAndKeepNulls = lsectionF(aScope)
+
+    return lresultsAndKeepNulls;
+}
+        
+var GetSectionFunction = function(abOTLTransform)
+{
+    //return the correct section function to use on the top level of this transform 
+    //
+    //Section functions take a scope and return a pair of (results list, keep-nulls).
+    //
+    //Note the real functions also take a bOTL transform, which is partially applied
+    //using a lambda.
+
+    var lfunction = null;
+
+    if (isObject(abOTLTransform))
+    {
+        // got a dict
+        if ("_type" in abOTLTransform)
+        {
+            var ltype = abOTLTransform["_type"];
+            if (ltype == "#")
+                lfunction = function (aScope) { return EvaluateFullSection(aScope, abOTLTransform) } 
+            else if (ltype == "object")
+                lfunction = function (aScope) { return EvaluateObjectSection(aScope, abOTLTransform) }
+            else if (ltype == "literal")
+                lfunction = function (aScope) { return EvaluateLiteralSection(aScope, abOTLTransform) }
+            else
+                lfunction = function (aScope) { return EvaluateObject(aScope, abOTLTransform) }
+        }
+        else
+            lfunction = function (aScope) { return EvaluateObject(aScope, abOTLTransform) }
+    }
+    else if (isArray(abOTLTransform))
+    {
+        lfunction = function (aScope) { return EvaluateList(aScope, abOTLTransform) }
+    }
+    else if (isString(abOTLTransform))
+    {
+        if (abOTLTransform.slice(0,1) == "#")
+            lfunction = function (aScope) { return EvaluateFullSection(aScope, {"_type": "#", "path": abOTLTransform.slice(1)}) };
+        else
+            lfunction = function (aScope) { return EvaluateLiteral(abOTLTransform) }
+    }
+    else
+    {
+        lfunction = function (aScope) { return EvaluateLiteral(abOTLTransform) }
+    }
+        
+    return lfunction
+}
+
+// Evaluate functions must return 
+// {"results": <a list of results>, "keepnulls": <boolean>}
+
+var EvaluateLiteral = function(abOTLLiteral)
+{
+    return {"results": [abOTLLiteral], "keepnulls": true};
+}
+
+var EvaluateFullSection = function(aScope, abOTLSection)
+{
+    var lpath = abOTLSection["path"] || "@";
+    var lscopeid = abOTLSection["scope"] || null;
+    var lhastransform = "transform" in abOTLSection;
+    var ltransform = abOTLSection["transform"] || null
+    var lhasnulltransform = "nulltransform" in abOTLSection;
+    var lnulltransform = abOTLSection["nulltransform"] || null;
+    var lkeepnulls = "nulls" in abOTLSection ? abOTLSection["nulls"] : true;
+    
+    //1: Evaluate the path
+    var lscope;
+    if (lpath.slice(0,1) == "$")
+        // this is a bit of a hack. I'd like to be able to tell jsonpath to use the attribute "$" as the root,
+        // but the library doesn't support that.
+        lscope = aScope["$"];
+    else
+        lscope = aScope;
+
+    var lselections = jsonPath(lscope, lpath);
+//     except Exception, ex:
+//         raise Exception("Failed to parse '%s': %s" % (lpath, repr(ex)))
+
+    var lresults = []
+        
+    if (isArray(lselections) && lselections.length)
+    {
+        // here we've got some results
+        if (lhastransform)
+        {
+            // need to transform all items in selection
+            for (var lselection of lselections)
+            {
+                // set up scope for transform, store 
+                // settings for restoration afterwards
+                var lprev_at = aScope["@"]
+                var lprev_scopeid;
+                aScope["@"] = lselection
+                if (lscopeid)
+                {
+                    lprev_scopeid = aScope[lscopeid]
+                    aScope[lscopeid] = lselection
+                }
+                
+                var lchildResults = _transform(aScope, ltransform);
+
+                lresults = lresults.concat(lchildResults["results"])
+                
+                // now restore the scope
+                aScope["@"] = lprev_at
+                if (lscopeid)
+                    aScope[lscopeid] = lprev_scopeid
+            }
+        }
+        else
+            lresults = lselections
+    }
+    else
+    {
+        // here we've got no results
+        if (lhasnulltransform)
+            lresults = _transform(aScope, lnulltransform)["results"]
+        else
+            lresults = []
+    }            
+    
+    if (!lkeepnulls)
+    {
+        var lfiltered = []
+
+        for (var lresult of lresults)
+        {
+            if (lresult != null)
+                lfiltered = lfiltered.concat([lresult])
+        }
+             
+        lresults = lfiltered
+    }
+        
+    return {"results": lresults, "keepnulls": lkeepnulls}
+}
+
+var EvaluateObjectSection = function(aScope, abOTLObjectSection)
+{
+    var retval = {"results": null, "keepnulls": true}
+
+    var lvalue = abOTLObjectSection["value"] || null;
+
+    if (isObject(lvalue))
+    {
+        lresult = {}
+        for (var lkey in lvalue)
+        {
+            if (lkey == "_type")
+                litem = lvalue[lkey]
+            else
+            {   
+                lchildResults = _transform(aScope, lvalue[lkey]);
+                if (lchildResults["results"].length)
+                    litem = lchildResults["results"][0]
+                else
+                    litem = null;
+            }
+
+            if (litem != null || lkeepnulls)
+                lresult[lkey] = litem
+        }
+
+        retval = {"results": [lresult], "keepnulls": true }
+    }
+        
+    return retval;
+}
+
+var EvaluateLiteralSection = function(aScope, abOTLObjectSection)
+{
+    return { "results": [abOTLObjectSection["value"] != null ? abOTLObjectSection["value"] : null], "keepnulls": true }
+}
+
+var EvaluateObject = function (aScope, abOTLObject)
+{
+    var ltransformedObject = {}
+    for (var lkey in abOTLObject)
+    {
+        ltransformedValue = _transform(aScope, abOTLObject[lkey]);
+
+        if (ltransformedValue["results"].length || ltransformedValue["keepnulls"])
+        {
+            ltransformedObject[lkey] = ltransformedValue["results"].length ? ltransformedValue["results"][0] : null;
+        }
+    }
+    
+    return {"results": [ltransformedObject], "keepnulls": true}
+}
+
+var EvaluateList = function(aScope, abOTLList)
+{
+    var lresults = [];
+    for (var litem of abOTLList)
+    { 
+        var lresult = _transform(aScope, litem)
+        lresults = lresults.concat(lresult["results"])
+    }
+    return {"results": [lresults], "keepnulls": true}
+}
+
+function isObject(obj) {
+  return !isArray(obj) && obj === Object(obj);
+}
+
+function isArray(obj) {
+  return Array.isArray(obj)
+}
+
+function isString(obj) {
+  return (typeof obj === 'string' || obj instanceof String);
+}
+
+/* JSONPath 0.8.5 - XPath for JSON
+ *
+ * Copyright (c) 2007 Stefan Goessner (goessner.net)
+ * Licensed under the MIT (MIT-LICENSE.txt) licence.
+ *
+ * Proposal of Chris Zyp goes into version 0.9.x
+ * Issue 7 resolved
+ */
+function jsonPath(obj, expr, arg) {
+   var P = {
+      resultType: arg && arg.resultType || "VALUE",
+      result: [],
+      normalize: function(expr) {
+         var subx = [];
+         return expr.replace(/[\['](\??\(.*?\))[\]']|\['(.*?)'\]/g, function($0,$1,$2){return "[#"+(subx.push($1||$2)-1)+"]";})  /* http://code.google.com/p/jsonpath/issues/detail?id=4 */
+                    .replace(/'?\.'?|\['?/g, ";")
+                    .replace(/;;;|;;/g, ";..;")
+                    .replace(/;$|'?\]|'$/g, "")
+                    .replace(/#([0-9]+)/g, function($0,$1){return subx[$1];});
+      },
+      asPath: function(path) {
+         var x = path.split(";"), p = "$";
+         for (var i=1,n=x.length; i<n; i++)
+            p += /^[0-9*]+$/.test(x[i]) ? ("["+x[i]+"]") : ("['"+x[i]+"']");
+         return p;
+      },
+      store: function(p, v) {
+         if (p) P.result[P.result.length] = P.resultType == "PATH" ? P.asPath(p) : v;
+         return !!p;
+      },
+      trace: function(expr, val, path) {
+         if (expr !== "") {
+            var x = expr.split(";"), loc = x.shift();
+            x = x.join(";");
+            if (val && val.hasOwnProperty(loc))
+               P.trace(x, val[loc], path + ";" + loc);
+            else if (loc === "*")
+               P.walk(loc, x, val, path, function(m,l,x,v,p) { P.trace(m+";"+x,v,p); });
+            else if (loc === "..") {
+               P.trace(x, val, path);
+               P.walk(loc, x, val, path, function(m,l,x,v,p) { typeof v[m] === "object" && P.trace("..;"+x,v[m],p+";"+m); });
+            }
+            else if (/^\(.*?\)$/.test(loc)) // [(expr)]
+               P.trace(P.eval(loc, val, path.substr(path.lastIndexOf(";")+1))+";"+x, val, path);
+            else if (/^\?\(.*?\)$/.test(loc)) // [?(expr)]
+               P.walk(loc, x, val, path, function(m,l,x,v,p) { if (P.eval(l.replace(/^\?\((.*?)\)$/,"$1"), v instanceof Array ? v[m] : v, m)) P.trace(m+";"+x,v,p); }); // issue 5 resolved
+            else if (/^(-?[0-9]*):(-?[0-9]*):?([0-9]*)$/.test(loc)) // [start:end:step]  phyton slice syntax
+               P.slice(loc, x, val, path);
+            else if (/,/.test(loc)) { // [name1,name2,...]
+               for (var s=loc.split(/'?,'?/),i=0,n=s.length; i<n; i++)
+                  P.trace(s[i]+";"+x, val, path);
+            }
+         }
+         else
+            P.store(path, val);
+      },
+      walk: function(loc, expr, val, path, f) {
+         if (val instanceof Array) {
+            for (var i=0,n=val.length; i<n; i++)
+               if (i in val)
+                  f(i,loc,expr,val,path);
+         }
+         else if (typeof val === "object") {
+            for (var m in val)
+               if (val.hasOwnProperty(m))
+                  f(m,loc,expr,val,path);
+         }
+      },
+      slice: function(loc, expr, val, path) {
+         if (val instanceof Array) {
+            var len=val.length, start=0, end=len, step=1;
+            loc.replace(/^(-?[0-9]*):(-?[0-9]*):?(-?[0-9]*)$/g, function($0,$1,$2,$3){start=parseInt($1||start);end=parseInt($2||end);step=parseInt($3||step);});
+            start = (start < 0) ? Math.max(0,start+len) : Math.min(len,start);
+            end   = (end < 0)   ? Math.max(0,end+len)   : Math.min(len,end);
+            for (var i=start; i<end; i+=step)
+               P.trace(i+";"+expr, val, path);
+         }
+      },
+      eval: function(x, _v, _vname) {
+         try { return $ && _v && eval(x.replace(/(^|[^\\])@/g, "$1_v").replace(/\\@/g, "@")); }  // issue 7 : resolved ..
+         catch(e) { throw new SyntaxError("jsonPath: " + e.message + ": " + x.replace(/(^|[^\\])@/g, "$1_v").replace(/\\@/g, "@")); }  // issue 7 : resolved ..
+      }
+   };
+
+   var $ = obj;
+   if (expr && obj && (P.resultType == "VALUE" || P.resultType == "PATH")) {
+      P.trace(P.normalize(expr).replace(/^\$;?/,""), obj, "$");  // issue 6 resolved
+      return P.result.length ? P.result : false;
+   }
+} 
---
+++ testsData.js
@@ -0,0 +1,376 @@
+testsJson = {
+    "tests": [
+        ["nulls", null, null, null],
+        ["root", "freddo dict", "root", "freddo string"],
+        ["local", "freddo dict", "local", "freddo string"],
+        ["list", "freddo dict", "list", "freddo in list"],
+        ["object1", "freddo dict", "object1", "object1"],
+        ["synopsis", "source1", "synopsis", "synopsis" ],
+        ["basics", "source1", "basics", "basics" ],
+        ["sections1", "source1", "sections1", "source1" ],
+        ["sections2", "source1", "sections2", "races list"],
+        ["sections3", "source1", "sections3", "none string"],
+        ["sections4", "source1", "sections4", "races objs"],
+        ["brief1", "source1", "brief1", "source1"],
+        ["brief2", "source1", "brief2", "races list"],
+        ["brief4", "source1", "brief4", "races objs"],
+        ["scope", "source1", "scope", "scope"],
+        ["scope2", "source1", "scope2", "scope2"],
+        ["scope3", "source1", "scope3", "scope3"],
+        ["list2", "source1", "list2", "list2"],
+        ["context1", "source1", "context1", "context1"],
+        ["context2", "source1", "context2", "context2"],
+        ["context3", "source1", "context3", "context3"],
+        ["ObjectSection", "source1", "ObjectSection", "ObjectSection"],
+        ["LiteralSection", "source1", "LiteralSection", "LiteralSection"],
+        ["KeepNulls", "source1", "KeepNulls", "KeepNulls"],
+        ["DontKeepNulls", "source1", "DontKeepNulls", "DontKeepNulls"]
+    ],
+    "transforms": {
+        "root": "#$.name",
+        "local": "#@.name",
+        "list": [ "#@.name" ],
+        "object1": {'saying': ["#@.quote"], 'fullname': '#@.name'},
+        "synopsis": {
+          "characters": [
+            {
+              "_type": "#",
+              "path": "$.items[*]",
+              "transform": {
+                "fullname": "#@.name",
+                "saying": {
+                  "_type": "#",
+                  "path": "@.quote",
+                  "nulls": false
+                }
+              }
+            }
+          ],
+          "urls": [
+            "#$..website"
+          ]
+        },
+        "basics": {"urls": ["#$..website"]},
+        "sections1": {"_type": "#"},
+        "sections2": [
+         {
+          "_type": "#",
+          "path": "$.items[*].type"
+         }
+        ],
+        "sections3": {
+          "_type": "#",
+          "path": "$..things",
+          "nulltransform": "none"
+        },
+        "sections4": [
+         {
+          "_type": "#",
+          "path": "$.items[*].type",
+          "transform": {
+            "race": {
+              "_type": "#"
+            }
+          }
+         }
+        ],
+        "brief1": "#",
+        "brief2": [ "#$.items[*].type" ],
+        "brief4": [
+            {
+              "_type": "#",
+              "path": "$.items[*].type",
+              "transform": {
+                "race": "#"
+              }
+            }
+        ],
+        "scope": [
+         {
+          "_type": "#",
+          "path": "$.items[*]",
+          "scope": "item",
+          "transform": 
+            {
+                "_type": "#",
+                "path": "@..images",
+                "transform": {
+                  "name": "#item.name",
+                  "pics": [
+                    {
+                      "_type": "#",
+                      "path": "@" 
+                    }
+                  ]
+                }
+            }
+         }
+        ],
+        "scope2": [
+         {
+          "_type": "#",
+          "path": "$.items[*]",
+          "scope": "item",
+          "transform": 
+            {
+                "_type": "#",
+                "path": "@..images",
+                "transform": {
+                  "name": "#item.name",
+                  "pics": [
+                    {
+                      "_type": "#",
+                      "path": "@" 
+                    }
+                  ]
+                },
+                "nulltransform": {
+                  "name": "#item.name",
+                  "nopics": true
+                }
+            }
+         }
+        ],
+        "scope3": [
+            {
+                "_type": "#",
+                "path": "$.items[*]",
+                "scope": "item",
+                "transform": 
+                {
+                    "name": "#@.name",
+                    "pics": {
+                        "_type": "#",
+                        "path": "@..images",
+                        "nulls": false
+                    }
+                }
+            }
+        ],
+        "list2": [
+            {
+              "_type": "#",
+              "path": "@..images",
+              "transform": {
+                "name": "XX",
+                "pics": [
+                    {
+                      "_type": "#",
+                      "path": "@[*]",
+                      "nulls": false
+                    }
+                ]
+              }
+            }
+        ],
+        "context1": [ "#$..website", "#$..images[*]" ],
+        "context2": "#$..website",
+        "context3": {
+          "stuff": {
+            "_type": "#",
+            "path": "$.stuff"
+          },
+          "thing": {
+            "_type": "#",
+            "path": "$.thing",
+            "nulls": false
+          }
+        },
+        "ObjectSection": {
+          "_type": "object",
+          "value": {
+            "_type": "#",
+            "transform": {
+              "names": ["#$..name"]
+            }
+          }
+        },
+        "LiteralSection": {
+          "_type": "literal",
+          "value": {
+            "_type": "#",
+            "transform": {
+              "names": ["#$..name"]
+            }
+          }
+        },
+        "KeepNulls": [{
+            "_type": "#",
+            "path": "$..items[*]",
+            "transform": {
+                "_type": "#",
+                "path": "@..quote",
+                "transform": {
+                    "quote": "#@"
+                },
+                "nulltransform": null
+            },
+            "nulls": true
+        }],
+        "DontKeepNulls": [{
+            "_type": "#",
+            "path": "$..items[*]",
+            "transform": {
+                "_type": "#",
+                "path": "@..quote",
+                "transform": {
+                    "quote": "#@"
+                },
+                "nulltransform": null
+            },
+            "nulls": false
+        }]
+    },
+    "sources": {
+        "freddo string": "Freddo",
+        "freddo in list": ["Freddo"],
+        "object1": {'saying': [], 'fullname': "Freddo"},
+        "synopsis": {
+              "characters": [
+                {
+                  "fullname": "Ambassador Kosh",
+                  "saying": "Yes"
+                },
+                {
+                  "fullname": "Worf"
+                },
+                {
+                  "fullname": "Stanley Tweedle"
+                }
+              ],
+              "urls": [
+                "http://babylon5.wikia.com/wiki/Kosh",
+                "https://en.wikipedia.org/wiki/Worf",
+                "http://lexx.wikia.com/wiki/Stanley_Tweedle"
+              ]
+            },
+        "basics": {
+              "urls": [
+                "http://babylon5.wikia.com/wiki/Kosh",
+                "https://en.wikipedia.org/wiki/Worf",
+                "http://lexx.wikia.com/wiki/Stanley_Tweedle"
+              ]
+            },
+        "freddo dict": {"name": "Freddo"},
+        "source1": {
+          "cursor": "8b08006b-963a-6909-c132-cc618cd4b352",
+          "more": true,
+          "items": [
+            {
+              "name": "Ambassador Kosh",
+              "type": "Vorlonn",
+              "resources": {
+                "website": "http://babylon5.wikia.com/wiki/Kosh",
+                "quote": "Yes"
+              }
+            },
+            {
+              "name": "Worf",
+              "type": "Klingon",
+              "resources": {
+                "website": "https://en.wikipedia.org/wiki/Worf",
+                "images": [
+                  "https://en.wikipedia.org/wiki/File:WorfTNG.jpg",
+                  "http://example.com/worf.jpg"
+                ]
+              }
+            },
+            {
+              "name": "Stanley Tweedle",
+              "type": "Human",
+              "resources": {
+                "website": "http://lexx.wikia.com/wiki/Stanley_Tweedle"
+              }
+            }
+          ]
+        },
+        "races list": ["Vorlonn", "Klingon", "Human"],
+        "none string": "none",
+        "races objs": [
+          { "race": "Vorlonn" },
+          { "race": "Klingon" },
+          { "race": "Human" }
+        ],
+        "scope": [
+          {
+            "name": "Worf",
+            "pics": [
+              [
+                "https://en.wikipedia.org/wiki/File:WorfTNG.jpg",
+                "http://example.com/worf.jpg"
+              ]
+            ]
+          }
+        ],
+        "scope2": [
+          {
+            "name": "Ambassador Kosh",
+            "nopics": true
+          },
+          {
+            "name": "Worf",
+            "pics": [
+              "https://en.wikipedia.org/wiki/File:WorfTNG.jpg",
+              "http://example.com/worf.jpg"
+            ]
+          },
+          {
+            "name": "Stanley Tweedle",
+            "nopics": true
+          }
+        ],
+        "scope3": [
+          {
+            "name": "Ambassador Kosh"
+          },
+          {
+            "name": "Worf",
+            "pics": [
+              "https://en.wikipedia.org/wiki/File:WorfTNG.jpg",
+              "http://example.com/worf.jpg"
+            ]
+          },
+          {
+            "name": "Stanley Tweedle"
+          }
+        ],
+        "list2": [
+          {
+            "name": "XX",
+            "pics": [
+              "https://en.wikipedia.org/wiki/File:WorfTNG.jpg",
+              "http://example.com/worf.jpg"
+            ]
+          }
+        ],
+        "context1": [
+          "http://babylon5.wikia.com/wiki/Kosh",
+          "https://en.wikipedia.org/wiki/Worf",
+          "http://lexx.wikia.com/wiki/Stanley_Tweedle",
+          "https://en.wikipedia.org/wiki/File:WorfTNG.jpg",
+          "http://example.com/worf.jpg"
+        ],
+        "context2": "http://babylon5.wikia.com/wiki/Kosh",
+        "context3": {
+          "stuff": null
+        },
+        "ObjectSection": {
+          "_type": "#",
+          "transform": {
+            "names": [
+              "Ambassador Kosh",
+              "Worf",
+              "Stanley Tweedle"
+            ]
+          }
+        },
+        "LiteralSection": {
+          "_type": "#",
+          "transform": {
+            "names": ["#$..name"]
+          }
+        },
+        "KeepNulls": [{'quote': 'Yes'}, null, null],
+        "DontKeepNulls": [{'quote': 'Yes'}]
+    }
+}
